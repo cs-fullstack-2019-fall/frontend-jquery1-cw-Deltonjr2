6e9a71675ac4a42a55a8c9bfb3368ec5685a44f1
autumn-ragland <raglanda998@gmail.com>
2019-09-12 20:25:36 -0500

    Graded 09-12-2019 20:25:14

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 //     // alert("Locked and Loaded");
 // });
 
+// !! : set classes using JQuery 
+
 $("#myElement").click(function(){
     alert("You clicked me!");
 
 });
 
+// !! : this does not meet an reuirement for exercise 1 
 setInterval(function() {
 $("#myElement").hide();
  setTimeout(function() {
